a6bd900cf9b451ced6d1e7d4372bf584bb01769f
edsont8 <edsont8@hotmail.com>
2021-07-09 08:21:58 -0300

    moved menu and footer to another directory

--- src/app/navegacao/main-nav/main-nav.component.ts
+++ src/app/navegacao/main-nav/main-nav.component.ts
@@ -1,4 +1,4 @@
-import { LoaderService } from './../loader/loader.service';
+import { LoaderService } from '../../loader/loader.service';
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
