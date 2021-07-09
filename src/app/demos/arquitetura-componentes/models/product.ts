17fd17216df15df509640584e1678decefcd4366
edsont8 <edsont8@hotmail.com>
2021-07-09 13:54:39 -0300

    new product card

---
+++ src/app/demos/arquitetura-componentes/models/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+    id: string;
+    name: string;
+    price: string;
+    saleOff: boolean;
+    settlementPrice: string;
+    description: string;
+    image: string;
+}
--- src/app/demos/arquitetura-componentes/product.module.ts
+++ src/app/demos/arquitetura-componentes/product.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from "@angular/common";
 import { ProductDashboardComponent } from './product-dashboard/product-dashboard.component';
 import { ProductRoutingModule } from './product.route';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+
 @NgModule({
     declarations: [
         ProductDashboardComponent
